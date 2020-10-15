18613d626f27ff3eb030fda4da48ffd2649bb8fd
theborakompanioni <theborakompanioni+github@gmail.com>
2020-10-15 21:30:26 +0200

    add more node info logging in jsonrpc client example app
    Signed-off-by: theborakompanioni <theborakompanioni+github@gmail.com>

--- bitcoin-jsonrpc-client-example-application/src/main/java/org/tbk/bitcoin/client/example/BitcoinClientApplicationConfig.java
+++ bitcoin-jsonrpc-client-example-application/src/main/java/org/tbk/bitcoin/client/example/BitcoinClientApplicationConfig.java
@@ -1,8 +1,12 @@
 package org.tbk.bitcoin.client.example;
 
+import com.google.common.base.Stopwatch;
 import com.msgilligan.bitcoinj.json.pojo.BlockChainInfo;
+import com.msgilligan.bitcoinj.json.pojo.NetworkInfo;
+import com.msgilligan.bitcoinj.json.pojo.UnspentOutput;
 import com.msgilligan.bitcoinj.rpc.BitcoinClient;
 import lombok.extern.slf4j.Slf4j;
+import org.bitcoinj.core.Block;
 import org.bitcoinj.core.Coin;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -38,20 +43,44 @@ public class BitcoinClientApplicationConfig {
     @Bean
     public CommandLineRunner printBitcoinNodeInfo(TaskScheduler scheduler, BitcoinClient bitcoinClient) {
         Runnable task = () -> {
+            Stopwatch stopwatch = Stopwatch.createStarted();
             try {
                 BlockChainInfo blockChainInfo = bitcoinClient.getBlockChainInfo();
+                log.info("'getBlockChainInfo' after {}", stopwatch);
                 log.info("Chain: {}", blockChainInfo.getChain());
                 log.info("Difficulty: {}", blockChainInfo.getDifficulty());
                 log.info("Blocks: {}", blockChainInfo.getBlocks());
                 log.info("BestBlockHash: {}", blockChainInfo.getBestBlockHash());
+                log.info("Headers: {}", blockChainInfo.getHeaders());
+                log.info("VerificationProgress: {}", blockChainInfo.getVerificationProgress());
 
                 Integer blockCount = bitcoinClient.getBlockCount();
+                log.info("'getBlockCount' after {}", stopwatch);
                 log.info("Block count: {}", blockCount);
 
+                NetworkInfo networkInfo = bitcoinClient.getNetworkInfo();
+                log.info("'getNetworkInfo' after {}", stopwatch);
+                log.info("Version: {} ({})", networkInfo.getVersion(), networkInfo.getSubVersion());
+
                 Coin balance = bitcoinClient.getBalance();
+                log.info("'getBalance' after {}", stopwatch);
                 log.info("Balance: {}", balance);
+
+                Block block0 = bitcoinClient.getBlock(0);
+                log.info("'getBlock(0)' after {}", stopwatch);
+                log.info("block0 Time: {}", block0.getTime());
+
+                Block blockBest = bitcoinClient.getBlock(blockChainInfo.getBestBlockHash());
+                log.info("'getBlock(best)' after {}", stopwatch);
+                log.info("blockBest Time: {}", blockBest.getTime());
+
+                final List<UnspentOutput> unspentOutputs = bitcoinClient.listUnspent();
+                log.info("'listUnspent' after {}", stopwatch);
+                log.info("UnspentOutputs: {}", unspentOutputs);
             } catch (Exception e) {
                 log.error("", e);
+            } finally {
+                stopwatch.stop();
             }
         };
 
