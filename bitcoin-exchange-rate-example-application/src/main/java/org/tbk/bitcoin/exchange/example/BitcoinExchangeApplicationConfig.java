444b8b836b09f484fa45c62f8dd6ddfe41c602be
theborakompanioni <theborakompanioni+github@gmail.com>
2020-10-24 12:49:49 +0200

    move creation of jackson mapper builder to WebMvcConfigurer
    Signed-off-by: theborakompanioni <theborakompanioni+github@gmail.com>

--- bitcoin-exchange-rate-example-application/src/main/java/org/tbk/bitcoin/exchange/example/BitcoinExchangeApplicationConfig.java
+++ bitcoin-exchange-rate-example-application/src/main/java/org/tbk/bitcoin/exchange/example/BitcoinExchangeApplicationConfig.java
@@ -1,7 +1,5 @@
 package org.tbk.bitcoin.exchange.example;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
@@ -9,10 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.tbk.bitcoin.exchange.config.BitcoinExchangeAutoConfiguration;
-import org.zalando.jackson.datatype.money.MoneyModule;
 
 import java.util.Arrays;
 
@@ -21,20 +17,6 @@ import java.util.Arrays;
 @EnableScheduling
 @Import(BitcoinExchangeAutoConfiguration.class)
 public class BitcoinExchangeApplicationConfig {
-
-    @Bean
-    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-        MoneyModule moneyModule = new MoneyModule()
-                .withDefaultFormatting()
-                .withQuotedDecimalNumbers();
-
-        return new Jackson2ObjectMapperBuilder()
-                .modulesToInstall(moneyModule)
-                .serializationInclusion(JsonInclude.Include.NON_NULL)
-                .featuresToEnable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
-                .indentOutput(true)
-                .failOnUnknownProperties(false);
-    }
 
     @Bean
     @Profile({"debug"})
--- bitcoin-exchange-rate-example-application/src/main/java/org/tbk/bitcoin/exchange/example/BitcoinExchangeApplicationWebMvcConfigurer.java
+++ bitcoin-exchange-rate-example-application/src/main/java/org/tbk/bitcoin/exchange/example/BitcoinExchangeApplicationWebMvcConfigurer.java
@@ -1,9 +1,14 @@
 package org.tbk.bitcoin.exchange.example;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.zalando.jackson.datatype.money.MoneyModule;
 
 @EnableWebMvc
 @Configuration
@@ -21,4 +26,19 @@ public class BitcoinExchangeApplicationWebMvcConfigurer implements WebMvcConfigu
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        MoneyModule moneyModule = new MoneyModule()
+                .withDefaultFormatting()
+                .withQuotedDecimalNumbers();
+
+        return new Jackson2ObjectMapperBuilder()
+                .modulesToInstall(moneyModule)
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .featuresToEnable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
+                .indentOutput(true)
+                .failOnUnknownProperties(false);
+    }
+
 }
