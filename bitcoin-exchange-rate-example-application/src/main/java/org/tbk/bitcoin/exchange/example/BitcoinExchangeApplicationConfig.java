c8c516b76153f712b3832a5b6fd72c47456769d9
theborakompanioni <theborakompanioni+github@gmail.com>
2020-10-26 20:17:22 +0100

    use 'xchange-jsr354-starter' module in example app

--- bitcoin-exchange-rate-example-application/src/main/java/org/tbk/bitcoin/exchange/example/BitcoinExchangeApplicationConfig.java
+++ bitcoin-exchange-rate-example-application/src/main/java/org/tbk/bitcoin/exchange/example/BitcoinExchangeApplicationConfig.java
@@ -5,18 +5,50 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.tbk.bitcoin.exchange.config.BitcoinExchangeAutoConfiguration;
+import org.tbk.bitcoin.exchange.BitcoinStandardExchangeRateProvider;
+import org.tbk.xchange.jsr354.XChangeExchangeRateProvider;
 
+import javax.money.Monetary;
+import javax.money.convert.ConversionQueryBuilder;
+import javax.money.convert.ExchangeRate;
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Configuration
 @EnableScheduling
-@Import(BitcoinExchangeAutoConfiguration.class)
 public class BitcoinExchangeApplicationConfig {
+
+    @Bean
+    public BitcoinStandardExchangeRateProvider bitcoinStandardExchangeRateProvider() {
+        return new BitcoinStandardExchangeRateProvider();
+    }
+
+    @Bean
+    public CommandLineRunner exchangeRateDemoRunner(List<XChangeExchangeRateProvider> XChangeExchangeRateProviders) {
+        return args -> {
+            if (XChangeExchangeRateProviders.isEmpty()) {
+                log.warn("No XChangeExchangeRateProviders found.");
+                return;
+            }
+
+            XChangeExchangeRateProviders.forEach(xChangeExchangeRateProvider -> {
+                log.info("======================================================");
+                log.info("Provider: {}", xChangeExchangeRateProvider.getContext());
+
+                ConversionQueryBuilder conversionQueryBuilder = ConversionQueryBuilder.of()
+                        .setBaseCurrency(Monetary.getCurrency("BTC"))
+                        .setTermCurrency(Monetary.getCurrency("USD"));
+
+                final ExchangeRate exchangeRate = xChangeExchangeRateProvider.getExchangeRate(conversionQueryBuilder.build());
+
+                log.info("exchangeRate: {}", exchangeRate);
+            });
+            log.info("======================================================");
+        };
+    }
 
     @Bean
     @Profile({"debug"})
