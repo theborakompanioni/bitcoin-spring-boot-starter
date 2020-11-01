f94b205a29df2a3546b17769739daa16bc018bdf
theborakompanioni <theborakompanioni+github@gmail.com>
2020-11-01 23:03:21 +0100

    test: add test methods for btc->usd and usd->btc conversion

--- examples/bitcoin-exchange-rate-example-application/src/test/java/org/tbk/bitcoin/exchange/example/BitcoinExchangeApplicationTest.java
+++ examples/bitcoin-exchange-rate-example-application/src/test/java/org/tbk/bitcoin/exchange/example/BitcoinExchangeApplicationTest.java
@@ -1,5 +1,6 @@
 package org.tbk.bitcoin.exchange.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.javamoney.moneta.Money;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,10 +12,7 @@ import org.tbk.xchange.jsr354.XChangeExchangeRateProvider;
 
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
-import javax.money.convert.ConversionQuery;
-import javax.money.convert.ConversionQueryBuilder;
-import javax.money.convert.CurrencyConversion;
-import javax.money.convert.ExchangeRate;
+import javax.money.convert.*;
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.Map;
@@ -22,6 +20,7 @@ import java.util.Map;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BitcoinExchangeApplicationTest {
@@ -35,7 +34,35 @@ public class BitcoinExchangeApplicationTest {
     }
 
     @Test
-    public void canFetchExchangeRatesForBitcoin() {
+    public void dollarsCanBeExchangedForBitcoin() {
+        CurrencyConversion btcToUsdConversion = MonetaryConversions.getConversion(ConversionQueryBuilder.of()
+                .setBaseCurrency(Monetary.getCurrency("USD"))
+                .setTermCurrency(Monetary.getCurrency("BTC"))
+                .build());
+
+        Money singleDollar = Money.of(BigDecimal.ONE, "USD");
+        Money singleDollarInBtc = singleDollar.with(btcToUsdConversion);
+
+        assertThat("value is in BTC", singleDollarInBtc.getCurrency().getCurrencyCode(), is("BTC"));
+        assertThat("value is greater than zero", singleDollarInBtc.isPositive(), is(true));
+    }
+
+    @Test
+    public void bitcoinCanBeExchangeToDollars() {
+        CurrencyConversion btcToUsdConversion = MonetaryConversions.getConversion(ConversionQueryBuilder.of()
+                .setBaseCurrency(Monetary.getCurrency("BTC"))
+                .setTermCurrency(Monetary.getCurrency("USD"))
+                .build());
+
+        Money singleBitcoin = Money.of(BigDecimal.ONE, "BTC");
+        Money singleBitcoinInUsd = singleBitcoin.with(btcToUsdConversion);
+
+        assertThat("value is in USD", singleBitcoinInUsd.getCurrency().getCurrencyCode(), is("USD"));
+        assertThat("value is greater than zero", singleBitcoinInUsd.isPositive(), is(true));
+    }
+
+    @Test
+    public void canFetchAllExchangeRatesForBitcoin() {
         final Map<String, XChangeExchangeRateProvider> beansOfType = applicationContext.getBeansOfType(XChangeExchangeRateProvider.class);
 
         Collection<XChangeExchangeRateProvider> providers = beansOfType.values();
