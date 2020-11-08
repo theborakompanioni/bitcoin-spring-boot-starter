eafa50d022b475884e97042b45a91e153361817e
theborakompanioni <theborakompanioni+github@gmail.com>
2020-11-08 15:17:29 +0100

    refactor: take OP_RETURN into account when extracting addresses

--- examples/bitcoin-transaction-stats-example-application/src/main/java/org/tbk/bitcoin/txstats/example/util/MoreScripts.java
+++ examples/bitcoin-transaction-stats-example-application/src/main/java/org/tbk/bitcoin/txstats/example/util/MoreScripts.java
@@ -5,6 +5,7 @@ import org.bitcoinj.core.Address;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.script.Script;
 import org.bitcoinj.script.ScriptException;
+import org.bitcoinj.script.ScriptPattern;
 
 import java.util.Optional;
 
@@ -16,13 +17,18 @@ public final class MoreScripts {
     }
 
     public static Optional<Address> extractAddress(NetworkParameters networkParameters, Script script) {
+
         return Optional.ofNullable(script)
                 .flatMap(val -> {
+                    if (ScriptPattern.isOpReturn(script)) {
+                        return Optional.empty();
+                    }
+
                     try {
                         boolean forcePayToPubKey = true; // force public key to address - otherwise its null!
                         return Optional.ofNullable(val.getToAddress(networkParameters, forcePayToPubKey));
                     } catch (ScriptException se) {
-                        log.debug("Cannot extract address from script: {}: ", se.getMessage());
+                        log.debug("Cannot extract address from script: {}: {}", script, se.getMessage());
                         return Optional.empty();
                     }
                 });
