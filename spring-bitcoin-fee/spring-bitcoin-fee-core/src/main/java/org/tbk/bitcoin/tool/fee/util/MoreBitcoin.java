8a9513d6bc81426f8f9c40a032b282f38639b156
theborakompanioni <theborakompanioni+github@gmail.com>
2020-11-18 20:35:25 +0100

    fee: add btcToSatFactor to util class MoreBitcoin

--- spring-bitcoin-fee/spring-bitcoin-fee-core/src/main/java/org/tbk/bitcoin/tool/fee/util/MoreBitcoin.java
+++ spring-bitcoin-fee/spring-bitcoin-fee-core/src/main/java/org/tbk/bitcoin/tool/fee/util/MoreBitcoin.java
@@ -1,10 +1,12 @@
 package org.tbk.bitcoin.tool.fee.util;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 
 public final class MoreBitcoin {
     private static final Duration AVERAGE_BLOCK_DURATION = Duration.ofMinutes(10);
-    ;
+    private static final int FRACTION_DIGITS = 8;
+    private static final BigDecimal BTC_TO_SAT_FACTOR = BigDecimal.TEN.pow(FRACTION_DIGITS);
 
     private MoreBitcoin() {
         throw new UnsupportedOperationException();
@@ -12,5 +14,9 @@ public final class MoreBitcoin {
 
     public static Duration averageBlockDuration() {
         return AVERAGE_BLOCK_DURATION;
+    }
+
+    public static BigDecimal btcToSatFactor() {
+        return BTC_TO_SAT_FACTOR;
     }
 }
