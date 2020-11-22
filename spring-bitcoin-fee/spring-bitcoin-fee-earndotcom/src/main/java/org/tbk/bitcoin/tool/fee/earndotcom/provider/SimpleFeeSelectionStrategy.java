3134017d3f5d576dfcf4efbc199fafb76b81a4a2
theborakompanioni <theborakompanioni+github@gmail.com>
2020-11-22 23:03:57 +0100

    fix: use maxMinutes of highest entry as lower threshold in earndotcom module

--- spring-bitcoin-fee/spring-bitcoin-fee-earndotcom/src/main/java/org/tbk/bitcoin/tool/fee/earndotcom/provider/SimpleFeeSelectionStrategy.java
+++ spring-bitcoin-fee/spring-bitcoin-fee-earndotcom/src/main/java/org/tbk/bitcoin/tool/fee/earndotcom/provider/SimpleFeeSelectionStrategy.java
@@ -12,9 +12,12 @@ import java.util.stream.Collectors;
 public class SimpleFeeSelectionStrategy implements FeeSelectionStrategy {
 
     private static final Comparator<FeesSummaryEntry> comparator = Comparator
-            .comparingLong(FeesSummaryEntry::getMinDelay)
-            .thenComparingLong(FeesSummaryEntry::getMinFee)
-            .thenComparing(Comparator.comparingLong(FeesSummaryEntry::getMaxFee).reversed());
+            .comparingLong(FeesSummaryEntry::getMinFee)
+            .thenComparingLong(FeesSummaryEntry::getMaxFee)
+            .thenComparingLong(FeesSummaryEntry::getMaxMinutes)
+            .thenComparingLong(FeesSummaryEntry::getMinMinutes)
+            .thenComparingLong(FeesSummaryEntry::getMaxDelay)
+            .thenComparingLong(FeesSummaryEntry::getMinDelay);
 
     @Override
     public Optional<FeesSummaryEntry> select(FeeRecommendationRequest request,
@@ -22,13 +25,18 @@ public class SimpleFeeSelectionStrategy implements FeeSelectionStrategy {
 
         long requestedMinutes = request.getDurationTarget().toMinutes();
 
-        // earndotcom has a minium of "30" returned for "max_minutes"
+        List<FeesSummaryEntry> feeList = transactionFeesSummary.getFeeList();
+        if (feeList.isEmpty()) {
+            return Optional.empty();
+        }
+        // earndotcom has a certain minimum returned for "max_minutes" e.g. 25.
         // which is a sane thing to do because you can never be sure how long it takes for the next block to be found
-        long minutes = Math.max(requestedMinutes, 30L);
+        // but this means we have to filter value with this value beforehand.
+        FeesSummaryEntry lastEntry = feeList.get(feeList.size() - 1);
+        long minutes = Math.max(requestedMinutes, lastEntry.getMaxMinutes());
 
-        List<FeesSummaryEntry> eligibleEntries = transactionFeesSummary.getFeeList().stream()
+        List<FeesSummaryEntry> eligibleEntries = feeList.stream()
                 .filter(val -> val.getMaxMinutes() <= minutes)
-                .sorted(comparator)
                 .collect(Collectors.toList());
 
         final Optional<FeesSummaryEntry> summaryEntryOrEmpty;
