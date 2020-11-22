d72084b471c26d45aa2aa7fe78b5c806dc779c32
theborakompanioni <theborakompanioni+github@gmail.com>
2020-11-22 15:29:42 +0100

    fix: prevent blockchair provider from answering zero duration requests

--- spring-bitcoin-fee/spring-bitcoin-fee-blockchair/src/main/java/org/tbk/bitcoin/tool/fee/blockchair/BlockchairFeeProvider.java
+++ spring-bitcoin-fee/spring-bitcoin-fee-blockchair/src/main/java/org/tbk/bitcoin/tool/fee/blockchair/BlockchairFeeProvider.java
@@ -15,6 +15,7 @@ import static java.util.Objects.requireNonNull;
 public class BlockchairFeeProvider extends AbstractFeeProvider {
     // blockchair only delivery one value "suggested_transaction_fee_per_byte_sat".
     // compared with whatthefee.io this seems to be a suggestion for the next 3 blocks
+    // (blockchair itself says, this if for the very next block => seems to be broader than that)
     private static final Duration MAX_DURATION_TARGET = MoreBitcoin.averageBlockDuration(3);
 
     private static final ProviderInfo providerInfo = ProviderInfo.SimpleProviderInfo.builder()
@@ -33,6 +34,7 @@ public class BlockchairFeeProvider extends AbstractFeeProvider {
     @Override
     public boolean supports(FeeRecommendationRequest request) {
         return request.getDesiredConfidence().isEmpty() &&
+                !request.isTargetDurationZeroOrLess() &&
                 MAX_DURATION_TARGET.compareTo(request.getDurationTarget()) >= 0;
     }
 
