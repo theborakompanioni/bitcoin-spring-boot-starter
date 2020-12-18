79008d2cd296e181ac079fe72acb56a58537812b
theborakompanioni <theborakompanioni+github@gmail.com>
2020-12-18 15:14:58 +0100

    fix: allow all alphabet chars in common container name

--- spring-testcontainer/spring-testcontainer-core/src/main/java/org/tbk/spring/testcontainer/core/MoreTestcontainers.java
+++ spring-testcontainer/spring-testcontainer-core/src/main/java/org/tbk/spring/testcontainer/core/MoreTestcontainers.java
@@ -25,11 +25,11 @@ public final class MoreTestcontainers {
     @RequiredArgsConstructor
     public final static class TbkContainerCmdModifiers {
         private static final String digits1234567890 = "123456789";
-        private static final String abcdefghijklmnopqrsduvwxyz = "ABCDEFGHIJKLMNOPQRSDUVWXYZ";
+        private static final String abcdefghijklmnopqrstuvwxyz = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        private static final CharMatcher VALID_NAME_CHAR_MATCHER = CharMatcher.anyOf(digits1234567890)
-                .or(CharMatcher.anyOf(abcdefghijklmnopqrsduvwxyz.toUpperCase()))
-                .or(CharMatcher.anyOf(abcdefghijklmnopqrsduvwxyz.toLowerCase()))
+        private static final CharMatcher validNameCharMatcher = CharMatcher.anyOf(digits1234567890)
+                .or(CharMatcher.anyOf(abcdefghijklmnopqrstuvwxyz.toUpperCase()))
+                .or(CharMatcher.anyOf(abcdefghijklmnopqrstuvwxyz.toLowerCase()))
                 .or(CharMatcher.anyOf("_-"))
                 .precomputed();
 
@@ -39,7 +39,7 @@ public final class MoreTestcontainers {
         public Consumer<CreateContainerCmd> withName(String name) {
             requireNonNull(name);
 
-            String cleanName = VALID_NAME_CHAR_MATCHER.retainFrom(name);
+            String cleanName = validNameCharMatcher.retainFrom(name);
             if (cleanName.isBlank()) {
                 throw new IllegalArgumentException("'name' is empty after removing illegal chars - given: " + name);
             }
