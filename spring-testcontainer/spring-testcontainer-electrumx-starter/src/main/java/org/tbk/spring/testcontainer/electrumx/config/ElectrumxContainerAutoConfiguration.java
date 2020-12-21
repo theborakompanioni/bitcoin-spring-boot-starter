e7951885874b3a5249eb1e56eda522210b918d29
theborakompanioni <theborakompanioni+github@gmail.com>
2020-12-22 00:25:05 +0100

    refactor: remove unnecessary/unsupported autoconfig properties for electrumx

--- spring-testcontainer/spring-testcontainer-electrumx-starter/src/main/java/org/tbk/spring/testcontainer/electrumx/config/ElectrumxContainerAutoConfiguration.java
+++ spring-testcontainer/spring-testcontainer-electrumx-starter/src/main/java/org/tbk/spring/testcontainer/electrumx/config/ElectrumxContainerAutoConfiguration.java
@@ -46,21 +46,11 @@ public class ElectrumxContainerAutoConfiguration {
     public ElectrumxContainer<?> electrumxContainer(BitcoindContainer<?> bitcoindContainer) {
         String bitcoindHost = "host.testcontainers.internal";
 
-        List<String> commands = ImmutableList.<String>builder()
-                .addAll(buildCommandList())
-                // TODO: expose ports specified via auto configuration properties
-                // .add("--bitcoind.rpchost=" + bitcoindHost + ":" + bitcoindContainer.getMappedPort(18443))
-                // .add("--bitcoind.zmqpubrawblock=tcp://" + bitcoindHost + ":" + bitcoindContainer.getMappedPort(28332))
-                // .add("--bitcoind.zmqpubrawtx=tcp://" + bitcoindHost + ":" + bitcoindContainer.getMappedPort(28333))
-                .build();
-
         List<Integer> hardcodedStandardPorts = ImmutableList.<Integer>builder()
                 .add(8000)
                 .add(50001)
                 .add(50002)
                 .add(50004)
-                // .add(this.properties.getRpcport())
-                // .add(this.properties.getRestport())
                 .build();
 
         List<Integer> exposedPorts = ImmutableList.<Integer>builder()
@@ -98,7 +88,6 @@ public class ElectrumxContainerAutoConfiguration {
         ElectrumxContainer<?> electrumxContainer = new ElectrumxContainer<>(dockerImageName)
                 .withCreateContainerCmdModifier(MoreTestcontainers.cmdModifiers().withName(dockerContainerName))
                 .withExposedPorts(exposedPorts.toArray(new Integer[]{}))
-                .withCommand(commands.toArray(new String[]{}))
                 .withEnv(env)
                 .waitingFor(waitStrategy);
 
@@ -109,107 +98,4 @@ public class ElectrumxContainerAutoConfiguration {
 
         return electrumxContainer;
     }
-
-    /**
-     * Build command list.
-     * e.g. see https://github.com/lightningnetwork/lnd/blob/master/sample-lnd.conf
-     *
-     * @return
-     */
-    private List<String> buildCommandList() {
-
-        List<String> requiredCommands = ImmutableList.<String>builder()
-                .add("--noseedbackup") // so no create/unlock wallet is needed ("lncli unlock")
-                //.add("--listen=9735")
-                //.add("--externalip=127.0.0.1:9735")
-                .add("--rpclisten=0.0.0.0:" + this.properties.getRpcport())
-                .build();
-
-        List<String> optionalCommands = ImmutableList.<String>builder()
-                .add("--alias=" + this.properties.getCommandValueByKey("alias").orElse("tbk-lnd-testcontainer-regtest"))
-                .add("--color=" + this.properties.getCommandValueByKey("color").orElse("#eeeeee"))
-                .add("--debuglevel=" + this.properties.getCommandValueByKey("debuglevel").orElse("debug"))
-                .add("--trickledelay=" + this.properties.getCommandValueByKey("trickledelay").orElse("1000"))
-                .build();
-
-        List<String> overridingDefaultsCommands = ImmutableList.<String>builder()
-                .add("--maxpendingchannels=" + this.properties.getCommandValueByKey("maxpendingchannels").orElse("10"))
-                //.add("--autopilot.active=false") <-- fails with "bool flag `--autopilot.active' cannot have an argument"
-                //.add("protocol.wumbo-channels=1")
-                .build();
-
-        List<String> bitcoinCommands = ImmutableList.<String>builder()
-                .add("--bitcoin.active")
-                .add("--bitcoin.regtest")
-                .add("--bitcoin.node=bitcoind")
-                .add("--bitcoin.defaultchanconfs=" + this.properties.getCommandValueByKey("bitcoin.defaultchanconfs").orElse("1"))
-                .build();
-
-        ImmutableList.Builder<String> commandsBuilder = ImmutableList.<String>builder()
-                // .addAll(requiredCommands)
-                // .addAll(optionalCommands)
-                // .addAll(overridingDefaultsCommands)
-                // .addAll(bitcoinCommands)
-                ;
-
-        List<String> predefinedKeys = commandsBuilder.build().stream()
-                .map(ElectrumXConfigEntry::valueOf)
-                .flatMap(Optional::stream)
-                .map(ElectrumXConfigEntry::getName)
-                .collect(Collectors.toList());
-
-        List<String> userGivenCommands = this.properties.getCommands();
-        List<String> allowedUserGivenCommands = userGivenCommands.stream()
-                .map(ElectrumXConfigEntry::valueOf)
-                .flatMap(Optional::stream)
-                .filter(it -> !predefinedKeys.contains(it.getName()))
-                .map(ElectrumXConfigEntry::toCommandString)
-                .collect(Collectors.toList());
-
-        return commandsBuilder
-                .addAll(allowedUserGivenCommands)
-                .build();
-    }
-
-    @Value
-    @Builder
-    public static class ElectrumXConfigEntry {
-        public static Optional<ElectrumXConfigEntry> valueOf(String command) {
-            String commandPrefix = "--";
-            return Optional.ofNullable(command)
-                    .filter(it -> it.startsWith(commandPrefix))
-                    .map(it -> it.split(commandPrefix)[1])
-                    .map(it -> {
-                        boolean withoutValue = !it.contains("=");
-                        if (withoutValue) {
-                            return ElectrumXConfigEntry.builder()
-                                    .name(it)
-                                    .build();
-                        }
-
-                        String[] parts = it.split("=");
-
-                        return ElectrumXConfigEntry.builder()
-                                .name(parts[0])
-                                .value(parts[1])
-                                .build();
-                    });
-        }
-
-        @NonNull
-        String name;
-
-        String value;
-
-        public Optional<String> getValue() {
-            return Optional.ofNullable(value);
-        }
-
-        public String toCommandString() {
-            return "--" + this.name + getValue()
-                    .map(it -> "=" + it)
-                    .orElse("");
-        }
-    }
-
 }
--- spring-testcontainer/spring-testcontainer-electrumx-starter/src/main/java/org/tbk/spring/testcontainer/electrumx/config/ElectrumxContainerProperties.java
+++ spring-testcontainer/spring-testcontainer-electrumx-starter/src/main/java/org/tbk/spring/testcontainer/electrumx/config/ElectrumxContainerProperties.java
@@ -1,9 +1,6 @@
 package org.tbk.spring.testcontainer.electrumx.config;
 
-import com.google.common.annotations.Beta;
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.Errors;
@@ -12,10 +9,6 @@ import org.testcontainers.shaded.com.google.common.base.CharMatcher;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import static com.google.common.base.Preconditions.checkArgument;
 
 @Data
 @ConfigurationProperties(
@@ -23,32 +16,6 @@ import static com.google.common.base.Preconditions.checkArgument;
         ignoreUnknownFields = false
 )
 public class ElectrumxContainerProperties implements Validator {
-
-    /**
-     * a list of reserved commands
-     * <p>
-     * some commands must have predefined values during the bootstrapping of the container.
-     * e.g.
-     * - `--bitcoin.active`
-     * - `--bitcoin.regtest`
-     * - `--bitcoin.node=bitcoind`
-     * <p>
-     * these commands should be disallowed to be specified by a user.
-     * <p>
-     * this behaviour is subject to change. i do not like it.
-     */
-    @Beta
-    private static final Set<String> reservedCommands = ImmutableSet.<String>builder()
-            .add("restlisten")
-            .add("rpclisten")
-            .add("noseedbackup")
-            .add("bitcoin.active")
-            .add("bitcoin.regtest")
-            .add("bitcoin.node")
-            .add("bitcoin.rpchost")
-            .add("bitcoin.zmqpubrawblock")
-            .add("bitcoin.zmqpubrawtx")
-            .build();
 
     /**
      * Whether the client should be enabled
@@ -75,38 +42,13 @@ public class ElectrumxContainerProperties implements Validator {
      */
     private Integer rpcport;
 
-    private List<String> commands;
-
     private List<Integer> exposedPorts;
-
-    public List<String> getCommands() {
-        return commands == null ?
-                Collections.emptyList() :
-                ImmutableList.copyOf(commands);
-    }
 
     public List<Integer> getExposedPorts() {
         return exposedPorts == null ?
                 Collections.emptyList() :
                 ImmutableList.copyOf(exposedPorts);
     }
-
-    public Optional<String> getCommandValueByKey(String key) {
-        String commandWithPrefix = "--" + key;
-        return this.getCommands().stream()
-                .filter(it -> it.startsWith(commandWithPrefix))
-                .map(it -> {
-                    boolean withoutValue = commandWithPrefix.length() == it.length();
-                    if (withoutValue) {
-                        return "";
-                    }
-
-                    checkArgument('=' == it.charAt(commandWithPrefix.length()));
-                    return it.split(commandWithPrefix + "=")[1];
-                })
-                .findFirst();
-    }
-
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -117,11 +59,9 @@ public class ElectrumxContainerProperties implements Validator {
      * Validate the container properties.
      * <p>
      * Keep in mind that Testcontainers splits commands on whitespaces.
-     * This means, every property that is part of a command, must not contain
-     * whitespaces.
-     *
-     * @param target
-     * @param errors
+     * This means, every property that is part of a command, must not contain whitespaces.
+     * <p>
+     * {@inheritDoc}
      */
     @Override
     public void validate(Object target, Errors errors) {
@@ -149,37 +89,6 @@ public class ElectrumxContainerProperties implements Validator {
         } else if (containsWhitespaces(rpcpasswordValue)) {
             String errorMessage = "'rpcpass' must not contain whitespaces - unsupported value";
             errors.rejectValue("rpcpass", "rpcpassword.unsupported", errorMessage);
-        }
-
-        reservedCommands.stream()
-                .filter(val -> properties.getCommandValueByKey(val).isPresent())
-                .forEach(reservedKey -> {
-                    String errorMessage = String.format("'commands' entry must not contain key '%s'", reservedKey);
-                    errors.rejectValue("commands", "commands.reserved", errorMessage);
-                });
-
-        for (String command : properties.getCommands()) {
-            if (Strings.isNullOrEmpty(command)) {
-                String errorMessage = "'commands' entry must not be empty";
-                errors.rejectValue("commands", "commands.invalid", errorMessage);
-            } else {
-                boolean startsWithHyphen = command.startsWith("--");
-                if (!startsWithHyphen) {
-                    String errorMessage = String.format(
-                            "'commands' entry must start with '--': invalid value: '%s'",
-                            command
-                    );
-                    errors.rejectValue("commands", "commands.invalid", errorMessage);
-                }
-
-                if (containsWhitespaces(command)) {
-                    String errorMessage = String.format(
-                            "'commands' entry must not contain whitespaces: unsupported value: '%s'",
-                            command
-                    );
-                    errors.rejectValue("commands", "commands.unsupported", errorMessage);
-                }
-            }
         }
     }
 
