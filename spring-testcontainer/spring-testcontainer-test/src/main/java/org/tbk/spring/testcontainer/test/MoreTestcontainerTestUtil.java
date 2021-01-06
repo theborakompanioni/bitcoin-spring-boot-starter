401585a9a08f0fb459cbb6f1c055636e79d21586
theborakompanioni <theborakompanioni+github@gmail.com>
2021-01-06 13:10:09 +0100

    test: add module testcontainer-test

---
+++ spring-testcontainer/spring-testcontainer-test/src/main/java/org/tbk/spring/testcontainer/test/MoreTestcontainerTestUtil.java
@@ -0,0 +1,32 @@
+package org.tbk.spring.testcontainer.test;
+
+import org.testcontainers.containers.ContainerState;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public final class MoreTestcontainerTestUtil {
+    private MoreTestcontainerTestUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Flux<Boolean> ranForMinimumDuration(ContainerState containerState) {
+        return ranForMinimumDuration(containerState, Duration.ofSeconds(3));
+    }
+
+    public static Flux<Boolean> ranForMinimumDuration(ContainerState containerState, Duration timeoutDuration) {
+        return ranForMinimumDuration(containerState, timeoutDuration, Duration.ofMillis(10));
+    }
+
+    public static Flux<Boolean> ranForMinimumDuration(ContainerState containerState, Duration timeoutDuration, Duration checkDuration) {
+        checkArgument(timeoutDuration.compareTo(checkDuration) >= 0, "'timeoutDuration' must not be less than 'checkDuration'");
+
+        return Flux.interval(checkDuration)
+                .map(foo -> containerState.isRunning())
+                .filter(running -> !running)
+                .timeout(timeoutDuration, Flux.just(true));
+    }
+
+}
