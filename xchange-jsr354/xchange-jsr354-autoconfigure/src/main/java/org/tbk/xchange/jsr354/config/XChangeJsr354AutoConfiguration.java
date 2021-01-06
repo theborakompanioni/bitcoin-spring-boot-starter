a9f369d10cc7a84bfb685987c3a570c86efd2f5e
theborakompanioni <theborakompanioni+github@gmail.com>
2021-01-06 22:43:18 +0100

    refactor: split Exchange and ExchangeRateProvider bean creation in separate configs

--- xchange-jsr354/xchange-jsr354-autoconfigure/src/main/java/org/tbk/xchange/jsr354/config/XChangeJsr354AutoConfiguration.java
+++ xchange-jsr354/xchange-jsr354-autoconfigure/src/main/java/org/tbk/xchange/jsr354/config/XChangeJsr354AutoConfiguration.java
@@ -4,18 +4,11 @@ import com.google.common.cache.CacheBuilderSpec;
 import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
 import org.knowm.xchange.Exchange;
-import org.knowm.xchange.ExchangeFactory;
-import org.knowm.xchange.bitfinex.BitfinexExchange;
-import org.knowm.xchange.bitstamp.BitstampExchange;
-import org.knowm.xchange.bittrex.BittrexExchange;
-import org.knowm.xchange.gemini.v1.GeminiExchange;
-import org.knowm.xchange.kraken.KrakenExchange;
-import org.knowm.xchange.therock.TheRockExchange;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.tbk.xchange.jsr354.CachingExchangeRateProvider;
@@ -23,6 +16,7 @@ import org.tbk.xchange.jsr354.XChangeExchangeRateProvider;
 import org.tbk.xchange.jsr354.cache.ConversionQueryCache;
 import org.tbk.xchange.jsr354.cache.ExchangeRateAvailabilityCache;
 import org.tbk.xchange.jsr354.cache.ExchangeRateCache;
+import org.tbk.xchange.spring.config.XChangeAutoConfiguration;
 
 import javax.money.convert.ExchangeRateProvider;
 import javax.money.convert.ProviderContext;
@@ -33,6 +27,7 @@ import static org.tbk.xchange.jsr354.MoreProviderContexts.createSimpleProviderCo
 @Slf4j
 @Configuration
 @ConditionalOnClass(Exchange.class)
+@AutoConfigureAfter(XChangeAutoConfiguration.class)
 public class XChangeJsr354AutoConfiguration {
     private static final CacheBuilderSpec exchangeRateAvailabilityCacheSpec = CacheBuilderSpec.parse(
             String.join(",", ImmutableList.<String>builder()
@@ -57,7 +52,7 @@ public class XChangeJsr354AutoConfiguration {
     }
 
     @Bean
-    public BeanFactoryPostProcessor exchangeRateProviderProcessor() {
+    public BeanFactoryPostProcessor xChangeJsr354ExchangeRateProviderBeanFactoryPostProcessor() {
         return new BeanFactoryPostProcessor() {
             @Override
             public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
@@ -90,74 +85,5 @@ public class XChangeJsr354AutoConfiguration {
                 return new CachingExchangeRateProvider(context, exchangeRateAvailabilityCache, exchangeRateCache);
             }
         };
-    }
-
-    @Configuration
-    @ConditionalOnClass(BitstampExchange.class)
-    public static class BitstampXChangeJsr354AutoConfiguration {
-
-        @Bean
-        @ConditionalOnMissingBean(BitstampExchange.class)
-        public BitstampExchange bitstampExchange() {
-            return ExchangeFactory.INSTANCE.createExchange(BitstampExchange.class);
-        }
-
-    }
-
-    @Configuration
-    @ConditionalOnClass(BitfinexExchange.class)
-    public static class BitfinexXChangeJsr354AutoConfiguration {
-
-        @Bean
-        @ConditionalOnMissingBean(BitfinexExchange.class)
-        public BitfinexExchange bitfinexExchange() {
-            return ExchangeFactory.INSTANCE.createExchange(BitfinexExchange.class);
-        }
-    }
-
-    @Configuration
-    @ConditionalOnClass(BittrexExchange.class)
-    public static class BittrexXChangeJsr354AutoConfiguration {
-
-        @Bean
-        @ConditionalOnMissingBean(BittrexExchange.class)
-        public BittrexExchange bittrexExchange() {
-            return ExchangeFactory.INSTANCE.createExchange(BittrexExchange.class);
-        }
-    }
-
-    @Configuration
-    @ConditionalOnClass(GeminiExchange.class)
-    public static class GeminiXChangeJsr354AutoConfiguration {
-
-        @Bean
-        @ConditionalOnMissingBean(GeminiExchange.class)
-        public GeminiExchange geminiExchange() {
-            return ExchangeFactory.INSTANCE.createExchange(GeminiExchange.class);
-        }
-
-    }
-
-    @Configuration
-    @ConditionalOnClass(KrakenExchange.class)
-    public static class KrakenXChangeJsr354AutoConfiguration {
-
-        @Bean
-        @ConditionalOnMissingBean(KrakenExchange.class)
-        public KrakenExchange krakenExchange() {
-            return ExchangeFactory.INSTANCE.createExchange(KrakenExchange.class);
-        }
-
-    }
-
-    @Configuration
-    @ConditionalOnClass(TheRockExchange.class)
-    public static class TheRockXChangeJsr354AutoConfiguration {
-
-        @Bean
-        @ConditionalOnMissingBean(TheRockExchange.class)
-        public TheRockExchange theRockExchange() {
-            return ExchangeFactory.INSTANCE.createExchange(TheRockExchange.class);
-        }
     }
 }
