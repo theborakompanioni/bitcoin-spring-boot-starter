9e3bb6edf80ee92532af784ba6138928d61aec84
theborakompanioni <theborakompanioni+github@gmail.com>
2021-01-13 01:08:27 +0100

    cleanup: move tests of tor module to integration tests

--- incubator/tbk-tor/tbk-tor-core/src/integTest/java/org/tbk/tor/NativeTorFactoryIntegrationTest.java
+++ incubator/tbk-tor/tbk-tor-core/src/integTest/java/org/tbk/tor/NativeTorFactoryIntegrationTest.java
@@ -1,10 +1,14 @@
 package org.tbk.tor;
 
+import org.berndpruenster.netlayer.tor.Control;
 import org.berndpruenster.netlayer.tor.NativeTor;
 import org.junit.Before;
 import org.junit.Test;
 
-public class NativeTorFactoryTest {
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class NativeTorFactoryIntegrationTest {
     // "www.torproject.org" as onion. taken from https://onion.torproject.org/ on 2020-01-13
     private static final String onionUrl = "expyuzz" + "4wqqyqh" + "j" + "n.on" + "ion";
 
@@ -16,9 +20,15 @@ public class NativeTorFactoryTest {
     }
 
     @Test
-    public void itShouldCreateTorSuccessfully() {
+    public void itShouldCheckOnionUrlAvailabilitySuccessfully() {
         NativeTor nativeTor = sut.create();
 
+        Control control = nativeTor.getControl();
+
+        boolean onionUrlIsAvailable = control.hsAvailable(onionUrl);
+
         nativeTor.shutdown();
+
+        assertThat("onion url is available", onionUrlIsAvailable, is(true));
     }
 }
