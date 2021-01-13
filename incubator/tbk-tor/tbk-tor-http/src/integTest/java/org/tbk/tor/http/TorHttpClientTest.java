a73346ce50ab80b954bddf8949da66993bb00fe3
theborakompanioni <theborakompanioni+github@gmail.com>
2021-01-13 03:11:51 +0100

    fix: use fake dns resolver in tor http client

---
+++ incubator/tbk-tor/tbk-tor-http/src/integTest/java/org/tbk/tor/http/TorHttpClientTest.java
@@ -0,0 +1,119 @@
+package org.tbk.tor.http;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.berndpruenster.netlayer.tor.NativeTor;
+import org.berndpruenster.netlayer.tor.TorCtlException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.tbk.tor.NativeTorFactory;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+@Slf4j
+public class TorHttpClientTest {
+    // "onion.torproject.org" as onion. taken from https://onion.torproject.org/ on 2020-01-13
+    private static final String ONION_URL = "http://yz7lpwfhhzcdyc5y.on" + "ion/";
+
+    private static final String CHECK_TOR_URL_HTTP = "http://check.torproject.org/";
+    private static final String CHECK_TOR_URL_HTTPS = "https://check.torproject.org/";
+
+    private CloseableHttpClient sut;
+
+    private NativeTor nativeTor;
+
+    @Before
+    public void setUp() throws TorCtlException {
+        NativeTorFactory torFactory = new NativeTorFactory();
+
+        this.nativeTor = torFactory.create().blockOptional(Duration.ofSeconds(30))
+                .orElseThrow(() -> new IllegalStateException("Could not start tor"));
+
+        HttpClientBuilder torHttpClientBuilder = SimpleTorHttpClientBuilder.tor(this.nativeTor);
+
+        this.sut = torHttpClientBuilder.build();
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            this.sut.close();
+        } catch (IOException e) {
+            log.warn("Error while closing http client in teardown phase", e);
+        }
+
+        this.nativeTor.shutdown();
+    }
+
+    @Test
+    public void testOnionWithTor() throws IOException {
+        HttpGet req = new HttpGet(ONION_URL);
+
+        try (CloseableHttpResponse rsp = this.sut.execute(req)) {
+            String body = EntityUtils.toString(rsp.getEntity(), StandardCharsets.UTF_8);
+
+            // body should contain a list of addresses - including the one we fetched from!
+            assertThat(body, containsString("onion.torproject.org"));
+            assertThat(body, containsString(ONION_URL));
+        }
+    }
+
+    @Test
+    public void testHttpWithTor() throws IOException {
+        List<String> urls = Lists.newArrayList(CHECK_TOR_URL_HTTP, CHECK_TOR_URL_HTTPS);
+
+        for (String url : urls) {
+            HttpGet req = new HttpGet(url);
+
+            try (CloseableHttpResponse rsp = this.sut.execute(req)) {
+                String body = EntityUtils.toString(rsp.getEntity(), StandardCharsets.UTF_8);
+
+                assertThat(body, containsString("Congratulations. This browser is configured to use Tor."));
+                assertThat(body, not(containsStringIgnoringCase("Sorry")));
+                assertThat(body, not(containsStringIgnoringCase("You are not using Tor")));
+            }
+        }
+    }
+
+    @Test
+    public void testOnionWithoutTor() throws IOException {
+        HttpGet req = new HttpGet(ONION_URL);
+
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            try (CloseableHttpResponse ignoredOnPurpose = client.execute(req)) {
+                Assert.fail("Should have thrown exception");
+            }
+        } catch (UnknownHostException e) {
+            assertThat(e.getMessage(), containsString("Name or service not known"));
+        }
+    }
+
+    @Test
+    public void testHttpWithoutTor() throws IOException {
+        HttpGet req = new HttpGet(CHECK_TOR_URL_HTTPS);
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+
+            try (CloseableHttpResponse rsp = client.execute(req)) {
+                String body = EntityUtils.toString(rsp.getEntity(), StandardCharsets.UTF_8);
+
+                assertThat(body, containsString("Sorry. You are not using Tor."));
+                assertThat(body, not(containsStringIgnoringCase("Congratulations")));
+                assertThat(body, not(containsStringIgnoringCase("This browser is configured to use Tor")));
+            }
+        }
+    }
+}
--- incubator/tbk-tor/tbk-tor-http/src/main/java/org/tbk/tor/http/SimpleTorHttpClientBuilder.java
+++ incubator/tbk-tor/tbk-tor-http/src/main/java/org/tbk/tor/http/SimpleTorHttpClientBuilder.java
@@ -4,8 +4,10 @@ import com.runjva.sourceforge.jsocks.protocol.Socks5Proxy;
 import org.apache.http.HttpHost;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.DnsResolver;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.DefaultHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
@@ -16,6 +18,7 @@ import org.apache.http.ssl.SSLContexts;
 import org.berndpruenster.netlayer.tor.Tor;
 import org.berndpruenster.netlayer.tor.TorCtlException;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import java.io.IOException;
 import java.net.*;
@@ -23,8 +26,8 @@ import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 
-public final class SimpleTorHttpClientBuilder {
 
+public final class SimpleTorHttpClientBuilder {
     public static HttpClientBuilder tor(Tor tor) throws TorCtlException {
         Socks5Proxy proxy = tor.getProxy();
         InetAddress inetAddress = proxy.getInetAddress();
@@ -35,15 +38,38 @@ public final class SimpleTorHttpClientBuilder {
     }
 
     public static HttpClientBuilder custom(Proxy proxy) {
+        DefaultHostnameVerifier defaultHostnameVerifier = new DefaultHostnameVerifier();
+        SSLContext sslContext = SSLContexts.createSystemDefault();
+        ProxySelectorSSLConnectionSocketFactory sslSocketFactory = new ProxySelectorSSLConnectionSocketFactory(proxy, sslContext, defaultHostnameVerifier);
+        ProxySelectorPlainConnectionSocketFactory plainSocketFactory = new ProxySelectorPlainConnectionSocketFactory(proxy);
+
         Registry<ConnectionSocketFactory> reg = RegistryBuilder.<ConnectionSocketFactory>create()
-                .register("http", new ProxySelectorPlainConnectionSocketFactory(proxy))
-                .register("https", new ProxySelectorSSLConnectionSocketFactory(proxy, SSLContexts.createSystemDefault()))
+                .register("http", plainSocketFactory)
+                .register("https", sslSocketFactory)
                 .build();
-        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(reg);
+
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(reg, new FakeDnsResolver());
+
         return HttpClients.custom()
                 .setConnectionManager(cm);
     }
 
+    private static Socket createSocket(HttpContext context, Proxy proxyOrNull) {
+        HttpHost httpTargetHost = (HttpHost) context.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
+        URI uri = URI.create(httpTargetHost.toURI());
+
+        Proxy proxy = Optional.ofNullable(proxyOrNull)
+                .orElseGet(() -> ProxySelector.getDefault().select(uri).iterator().next());
+
+        return new Socket(proxy);
+    }
+
+    /*
+     * It is very difficult to say to an apache http client to "not use my DNS servers while connecting through a proxy".
+     *
+     * Some code taken from:
+     * https://stackoverflow.com/questions/22937983/how-to-use-socks-5-proxy-with-apache-http-client-4/25203021#25203021
+     */
     private static class ProxySelectorPlainConnectionSocketFactory implements ConnectionSocketFactory {
 
         private final Proxy proxy;
@@ -59,15 +85,19 @@ public final class SimpleTorHttpClientBuilder {
 
         @Override
         public Socket connectSocket(int connectTimeout, Socket sock, HttpHost host, InetSocketAddress remoteAddress, InetSocketAddress localAddress, HttpContext context) throws IOException {
-            return PlainConnectionSocketFactory.INSTANCE.connectSocket(connectTimeout, sock, host, remoteAddress, localAddress, context);
+            InetSocketAddress unresolvedRemote = InetSocketAddress.createUnresolved(host.getHostName(), remoteAddress.getPort());
+
+            return PlainConnectionSocketFactory.INSTANCE.connectSocket(connectTimeout, sock, host, unresolvedRemote, localAddress, context);
         }
     }
 
     private static final class ProxySelectorSSLConnectionSocketFactory extends SSLConnectionSocketFactory {
         private final Proxy proxy;
 
-        ProxySelectorSSLConnectionSocketFactory(Proxy proxy, SSLContext sslContext) {
-            super(sslContext);
+        ProxySelectorSSLConnectionSocketFactory(Proxy proxy,
+                                                SSLContext sslContext,
+                                                HostnameVerifier hostnameVerifier) {
+            super(sslContext, hostnameVerifier);
             this.proxy = requireNonNull(proxy);
         }
 
@@ -75,15 +105,22 @@ public final class SimpleTorHttpClientBuilder {
         public Socket createSocket(HttpContext context) {
             return SimpleTorHttpClientBuilder.createSocket(context, this.proxy);
         }
+
+        @Override
+        public Socket connectSocket(int connectTimeout, Socket sock, HttpHost host, InetSocketAddress remoteAddress, InetSocketAddress localAddress, HttpContext context) throws IOException {
+            InetSocketAddress unresolvedRemote = InetSocketAddress.createUnresolved(host.getHostName(), remoteAddress.getPort());
+
+            return super.connectSocket(connectTimeout, sock, host, unresolvedRemote, localAddress, context);
+        }
     }
 
-    private static Socket createSocket(HttpContext context, Proxy proxyOrNull) {
-        HttpHost httpTargetHost = (HttpHost) context.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
-        URI uri = URI.create(httpTargetHost.toURI());
+    static class FakeDnsResolver implements DnsResolver {
+        // Return this fake DNS record for every request, we won't be using it
+        private static final InetAddress[] fakeDnsEntry = {InetAddress.getLoopbackAddress()};
 
-        Proxy proxy = Optional.ofNullable(proxyOrNull)
-                .orElseGet(() -> ProxySelector.getDefault().select(uri).iterator().next());
-
-        return new Socket(proxy);
+        @Override
+        public InetAddress[] resolve(String host) {
+            return fakeDnsEntry;
+        }
     }
 }
