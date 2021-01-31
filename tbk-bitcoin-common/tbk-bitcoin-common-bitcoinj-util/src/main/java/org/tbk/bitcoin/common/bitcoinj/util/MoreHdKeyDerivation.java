7d79d74e141f128323db803e7ad6551fb49248aa
theborakompanioni <theborakompanioni+github@gmail.com>
2021-01-31 14:35:17 +0100

    feature: add helper class MoreHdKeyDerivation for deriving deterministic keys

---
+++ tbk-bitcoin-common/tbk-bitcoin-common-bitcoinj-util/src/main/java/org/tbk/bitcoin/common/bitcoinj/util/MoreHdKeyDerivation.java
@@ -0,0 +1,49 @@
+package org.tbk.bitcoin.common.bitcoinj.util;
+
+import org.bitcoinj.crypto.ChildNumber;
+import org.bitcoinj.crypto.DeterministicKey;
+import org.bitcoinj.crypto.HDKeyDerivation;
+import org.bitcoinj.crypto.HDUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.requireNonNull;
+
+public final class MoreHdKeyDerivation {
+
+    private MoreHdKeyDerivation() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static DeterministicKey deriveChildKey(DeterministicKey parent, String path) {
+        requireNonNull(parent, "'parent' must not be null");
+        requireNonNull(path, "'path' must not be null");
+
+        return deriveChildKey(parent, HDUtils.parsePath(path));
+    }
+
+    public static DeterministicKey deriveChildKey(DeterministicKey parent, List<ChildNumber> childNumbers) {
+        requireNonNull(parent, "'parent' must not be null");
+        requireNonNull(childNumbers, "'childNumbers' must not be null");
+
+        if (childNumbers.isEmpty()) {
+            throw new IllegalStateException("Cannot derive child from empty list");
+        }
+
+        return deriveChildKeyRecursive(parent, childNumbers);
+    }
+
+    private static DeterministicKey deriveChildKeyRecursive(DeterministicKey parent, List<ChildNumber> childNumbers) {
+        if (childNumbers.isEmpty()) {
+            return parent;
+        }
+
+        ChildNumber first = childNumbers.get(0);
+        DeterministicKey childKey = HDKeyDerivation.deriveChildKey(parent, first);
+
+        List<ChildNumber> newChildNumbers = childNumbers.stream().skip(1).collect(Collectors.toList());
+
+        return deriveChildKeyRecursive(childKey, newChildNumbers);
+    }
+}
---
+++ tbk-bitcoin-common/tbk-bitcoin-common-bitcoinj-util/src/test/java/org/tbk/bitcoin/common/bitcoinj/util/MoreHdKeyDerivationTest.java
@@ -0,0 +1,119 @@
+package org.tbk.bitcoin.common.bitcoinj.util;
+
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.crypto.DeterministicKey;
+import org.bitcoinj.crypto.HDKeyDerivation;
+import org.bitcoinj.crypto.MnemonicCode;
+import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.script.Script;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MoreHdKeyDerivationTest {
+
+    private final NetworkParameters network = MainNetParams.get();
+
+    private final String mnemonicCode = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
+    private final List<String> mnemonicCodeList = Arrays.asList(mnemonicCode.split(" "));
+
+    private final String xpriv = "xprv9s21ZrQH143K3GJpoapnV8SFfukcVBSfeCficPSGfubmSFDxo1kuHnLisriDvSnRRuL2Qrg5ggqHKNVpxR86QEC8w35uxmGoggxtQTPvfUu";
+    private final String xpub = "xpub661MyMwAqRbcFkPHucMnrGNzDwb6teAX1RbKQmqtEF8kK3Z7LZ59qafCjB9eCRLiTVG3uxBxgKvRgbubRhqSKXnGGb1aoaqLrpMBDrVxga8";
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void itShouldCreateMainnetBech32AddressFromMnemonic() {
+        byte[] seed = MnemonicCode.toSeed(mnemonicCodeList, "");
+
+        DeterministicKey masterPrivateKey = HDKeyDerivation.createMasterPrivateKey(seed);
+        assertThat(masterPrivateKey.getPathAsString(), is("M"));
+
+        DeterministicKey xprivChildBip84 = MoreHdKeyDerivation.deriveChildKey(masterPrivateKey, "84H / 0H / 0H / 0 / 0");
+        assertThat(xprivChildBip84.getPathAsString(), is("M/84H/0H/0H/0/0"));
+
+        Address address = Address.fromKey(network, xprivChildBip84, Script.ScriptType.P2WPKH);
+        assertThat(address.toString(), is("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu"));
+    }
+
+    @Test
+    public void itShouldCreateMainnetBech32AddressFromXpriv() {
+        DeterministicKey xprivKey = DeterministicKey.deserializeB58(xpriv, network);
+        assertThat(xprivKey.getPathAsString(), is("M"));
+
+        DeterministicKey xprivChildBip84 = MoreHdKeyDerivation.deriveChildKey(xprivKey, "84H / 0H / 0H / 0 / 0");
+        assertThat(xprivChildBip84.getPathAsString(), is("M/84H/0H/0H/0/0"));
+
+        Address address = Address.fromKey(network, xprivChildBip84, Script.ScriptType.P2WPKH);
+        assertThat(address.toString(), is("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu"));
+    }
+
+    @Test
+    public void itShouldDeriveAddressesFromZpriv() {
+        // the zpriv of the path "84H / 0H / 0H" of mnemonic "abendon abendon ... about"
+        String zprivBip84 = "zprvAdG4iTXWBoARxkkzNpNh8r6Qag3irQB8PzEMkAFeTRXxHpbF9z4QgEvBRmfvqWvGp42t42nvgGpNgYSJA9iefm1yYNZKEm7z6qUWCroSQnE";
+
+        DeterministicKey zprivBip84Key = DeterministicKey.deserializeB58(zprivBip84, network);
+        assertThat(zprivBip84Key.getPathAsString(), is("M/0H"));
+
+        DeterministicKey zprivBip84KeyChild = MoreHdKeyDerivation.deriveChildKey(zprivBip84Key, "0 / 0");
+        assertThat(zprivBip84KeyChild.getPathAsString(), is("M/0H/0/0"));
+
+        Address address = Address.fromKey(network, zprivBip84KeyChild, Script.ScriptType.P2WPKH);
+        assertThat(address.toString(), is("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu"));
+    }
+
+    @Test
+    public void itShouldDeriveHardenedAddressesFromZpriv() {
+        // the zpriv of the path "84H / 0H / 0H" of mnemonic "abendon abendon ... about"
+        String zprivBip84 = "zprvAdG4iTXWBoARxkkzNpNh8r6Qag3irQB8PzEMkAFeTRXxHpbF9z4QgEvBRmfvqWvGp42t42nvgGpNgYSJA9iefm1yYNZKEm7z6qUWCroSQnE";
+
+        DeterministicKey zprivBip84Key = DeterministicKey.deserializeB58(zprivBip84, network);
+        assertThat(zprivBip84Key.getPathAsString(), is("M/0H"));
+
+        DeterministicKey xprivBip84KeyChild = MoreHdKeyDerivation.deriveChildKey(zprivBip84Key, "0H");
+        assertThat(xprivBip84KeyChild.getPathAsString(), is("M/0H/0H"));
+
+        Address address = Address.fromKey(network, xprivBip84KeyChild, Script.ScriptType.P2WPKH);
+        assertThat(address.toString(), is("bc1qfwuage805chdj3lykds088hgn7j47sundvhzr8"));
+    }
+
+    @Test
+    public void itShouldDeriveAddressesFromZpub() {
+        // zpub of "84H / 0H / 0H" of mnemonic "abendon abendon ... about"
+        String zpubBip84 = "zpub6rFR7y4Q2AijBEqTUquhVz398htDFrtymD9xYYfG1m4wAcvPhXNfE3EfH1r1ADqtfSdVCToUG868RvUUkgDKf31mGDtKsAYz2oz2AGutZYs";
+
+        DeterministicKey zpubBip84Key = DeterministicKey.deserializeB58(zpubBip84, network);
+        assertThat(zpubBip84Key.getPathAsString(), is("M/0H"));
+
+        DeterministicKey zpubBip84KeyChild = MoreHdKeyDerivation.deriveChildKey(zpubBip84Key, "0 / 0");
+        assertThat(zpubBip84KeyChild.getPathAsString(), is("M/0H/0/0"));
+
+        Address address = Address.fromKey(network, zpubBip84KeyChild, Script.ScriptType.P2WPKH);
+        assertThat(address.toString(), is("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu"));
+    }
+
+    @Test
+    public void itShouldFailToDeriveHardenedAddressesFromZpub() {
+        // zpub of "84H / 0H / 0H" of mnemonic "abendon abendon ... about"
+        String zpubBip84 = "zpub6rFR7y4Q2AijBEqTUquhVz398htDFrtymD9xYYfG1m4wAcvPhXNfE3EfH1r1ADqtfSdVCToUG868RvUUkgDKf31mGDtKsAYz2oz2AGutZYs";
+
+        DeterministicKey zpubBip84Key = DeterministicKey.deserializeB58(zpubBip84, network);
+        try {
+            DeterministicKey ignoredOnPurpose = MoreHdKeyDerivation.deriveChildKey(zpubBip84Key, "0H");
+
+            Assert.fail("Should have thrown exception");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), is("Hardened derivation is unsupported (0H)."));
+        }
+    }
+}
