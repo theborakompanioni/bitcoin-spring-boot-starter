02e5e3594971e387a0a1ae4f34848c0bf52f19b8
theborakompanioni <theborakompanioni+github@gmail.com>
2021-02-04 00:45:36 +0100

    refactor: add ability to aggregate fee responses in an reduce hook

--- bitcoin-fee/bitcoin-fee-estimate-core/src/main/java/org/tbk/bitcoin/tool/fee/AbstractFeeProvider.java
+++ bitcoin-fee/bitcoin-fee-estimate-core/src/main/java/org/tbk/bitcoin/tool/fee/AbstractFeeProvider.java
@@ -25,7 +25,7 @@ public abstract class AbstractFeeProvider implements FeeProvider {
 
     @Override
     public final Flux<FeeRecommendationResponse> request(FeeRecommendationRequest feeRecommendationRequest) {
-        return Flux.just(feeRecommendationRequest)
+        Flux<FeeRecommendationResponse> result = Flux.just(feeRecommendationRequest)
                 .filter(this::supports)
                 .flatMap(this::requestHook)
                 .flatMap(this::transformHook)
@@ -34,16 +34,39 @@ public abstract class AbstractFeeProvider implements FeeProvider {
                         .providerInfo(Optional.ofNullable(val.getProviderInfo()).orElseGet(this::info))
                         .build()
                 );
-    }
 
-    // TODO: make this method return FeeRecommendations only
-    protected abstract Flux<FeeRecommendationResponse> requestHook(FeeRecommendationRequest request);
-
-    protected Flux<FeeRecommendationResponse> transformHook(FeeRecommendationResponse result) {
-        return Flux.just(result);
+        return reduceHook(result);
     }
 
     protected ProviderInfo infoHook() {
         return this.providerInfo;
+    }
+
+    protected abstract Flux<FeeRecommendationResponse> requestHook(FeeRecommendationRequest request);
+
+    /**
+     * A hook to transform a single {@link FeeRecommendationResponse} response.
+     * Append information, filter, transform or add additional responses.
+     *
+     * Default behaviour is to return the unchanged value that has been provided.
+     *
+     * @param response the fee recommendation response
+     * @return the unchanged value that has been provided.
+     */
+    protected Flux<FeeRecommendationResponse> transformHook(FeeRecommendationResponse response) {
+        return Flux.just(response);
+    }
+
+    /**
+     * A hook to transform a stream of {@link FeeRecommendationResponse} responses.
+     * This is the place to e.g. aggregate results or derive statistical metrics.
+     *
+     * Default behaviour is to return the unchanged stream that has been provided.
+     *
+     * @param responses the fee recommendation responses
+     * @return the unchanged stream that has been provided.
+     */
+    protected Flux<FeeRecommendationResponse> reduceHook(Flux<FeeRecommendationResponse> responses) {
+        return responses;
     }
 }
