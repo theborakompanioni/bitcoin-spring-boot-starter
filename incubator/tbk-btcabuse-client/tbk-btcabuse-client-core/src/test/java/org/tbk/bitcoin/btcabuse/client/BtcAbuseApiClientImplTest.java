b5377e161a9f10b98bb2d4ec6f5ebbc92863ef4b
theborakompanioni <theborakompanioni+github@gmail.com>
2021-04-10 19:35:34 +0200

    refactor: replace wiremock with mockserver

--- incubator/tbk-btcabuse-client/tbk-btcabuse-client-core/src/test/java/org/tbk/bitcoin/btcabuse/client/BtcAbuseApiClientImplTest.java
+++ incubator/tbk-btcabuse-client/tbk-btcabuse-client-core/src/test/java/org/tbk/bitcoin/btcabuse/client/BtcAbuseApiClientImplTest.java
@@ -1,36 +1,54 @@
 package org.tbk.bitcoin.btcabuse.client;
 
-//import com.github.tomakehurst.wiremock.junit.WireMockRule;
-//import com.github.tomakehurst.wiremock.matching.StringValuePattern;
+import com.google.common.collect.ImmutableList;
+import lombok.Builder;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.junit.jupiter.MockServerExtension;
+import org.mockserver.model.Header;
+import org.mockserver.model.Parameter;
+import org.tbk.bitcoin.tool.btcabuse.CheckResponseDto;
 
-import org.junit.jupiter.api.Disabled;
+import static java.util.Objects.requireNonNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
-@Disabled("Currently needs wiremock which is not compatible with junit5 without wiremock-extension")
-public class BtcAbuseApiClientImplTest {
+@ExtendWith(MockServerExtension.class)
+class BtcAbuseApiClientImplTest {
 
-    /*
-    @org.junit.RuleRule
-    public WireMockRule wireMockRule = new WireMockRule(options().dynamicPort());
+    private static final String DUMMY_API_TOKEN = RandomStringUtils.randomAlphanumeric(10, 20);
+
+    private final MockServerClient client;
 
     private BtcAbuseApiClientImpl sut;
 
-    @BeforeEach
-    public void setup() {
-        String baseUrl = wireMockRule.baseUrl();
-        String apiToken = RandomStringUtils.randomAlphanumeric(10, 20);
+    BtcAbuseApiClientImplTest(MockServerClient client) {
+        this.client = requireNonNull(client);
 
-        this.sut = new BtcAbuseApiClientImpl(HttpClients.createDefault(), baseUrl, apiToken);
-
-        BitcoinAbuseWireMocked bitcoinAbuseWireMocked = BitcoinAbuseWireMocked.builder()
-                .apiToken(apiToken)
-                .wireMockRule(wireMockRule)
+        BitcoinAbuseMockedApiSetup bitcoinAbuseWireMocked = BitcoinAbuseMockedApiSetup.builder()
+                .client(client)
                 .build();
 
         bitcoinAbuseWireMocked.setupStub();
     }
 
+    @BeforeEach
+    void setup() {
+        String baseUrl = "http://localhost:" + client.getPort();
+
+        this.sut = new BtcAbuseApiClientImpl(HttpClients.createDefault(), baseUrl, DUMMY_API_TOKEN);
+
+    }
+
     @Test
-    public void itShouldCheckReportedAddressSuccessfully() {
+    void itShouldCheckReportedAddressSuccessfully() {
         CheckResponseDto check = this.sut.check("12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw");
 
         assertThat(check, is(notNullValue()));
@@ -39,7 +57,7 @@ public class BtcAbuseApiClientImplTest {
     }
 
     @Test
-    public void itShouldCheckUnreportedAddressSuccessfully() {
+    void itShouldCheckUnreportedAddressSuccessfully() {
         CheckResponseDto check = this.sut.check("1PaLmmeoe5Ktv613UGBCxCUZ27owv9Q6XY");
 
         assertThat(check, is(notNullValue()));
@@ -48,38 +66,37 @@ public class BtcAbuseApiClientImplTest {
     }
 
     @Builder
-    private static class BitcoinAbuseWireMocked {
-        private final WireMockRule wireMockRule;
-        private final String apiToken;
+    private static class BitcoinAbuseMockedApiSetup {
+        private final MockServerClient client;
 
         public void setupStub() {
             // "check" endpoint with reported address
-            wireMockRule.stubFor(get(urlPathEqualTo("/api/reports/check"))
-                    .withQueryParams(ImmutableMap.<String, StringValuePattern>builder()
-                            .put("address", equalTo("12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw"))
-                            .put("api_token", equalTo(apiToken))
+            client.when(request()
+                    .withPath("/api/reports/check")
+                    .withQueryStringParameters(ImmutableList.<Parameter>builder()
+                            .add(Parameter.param("address", "12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw"))
                             .build())
-                    .willReturn(aResponse()
-                            .withHeader("Content-Type", "application/json")
-                            .withStatus(200)
-                            .withBody("{\n" +
-                                    "  \"address\": \"12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw\",\n" +
-                                    "  \"count\": 2\n" +
-                                    "}")));
+            ).respond(response()
+                    .withHeader(Header.header("Content-Type", "application/json"))
+                    .withStatusCode(200)
+                    .withBody("{\n" +
+                            "  \"address\": \"12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw\",\n" +
+                            "  \"count\": 2\n" +
+                            "}"));
 
             // "check" endpoint with unreported address
-            wireMockRule.stubFor(get(urlPathEqualTo("/api/reports/check"))
-                    .withQueryParams(ImmutableMap.<String, StringValuePattern>builder()
-                            .put("address", equalTo("1PaLmmeoe5Ktv613UGBCxCUZ27owv9Q6XY"))
-                            .put("api_token", equalTo(apiToken))
+            client.when(request()
+                    .withPath("/api/reports/check")
+                    .withQueryStringParameters(ImmutableList.<Parameter>builder()
+                            .add(Parameter.param("address", "1PaLmmeoe5Ktv613UGBCxCUZ27owv9Q6XY"))
                             .build())
-                    .willReturn(aResponse()
-                            .withHeader("Content-Type", "application/json")
-                            .withStatus(200)
-                            .withBody("{\n" +
-                                    "  \"address\": \"1PaLmmeoe5Ktv613UGBCxCUZ27owv9Q6XY\",\n" +
-                                    "  \"count\": 0\n" +
-                                    "}")));
+            ).respond(response()
+                    .withHeader(Header.header("Content-Type", "application/json"))
+                    .withStatusCode(200)
+                    .withBody("{\n" +
+                            "  \"address\": \"1PaLmmeoe5Ktv613UGBCxCUZ27owv9Q6XY\",\n" +
+                            "  \"count\": 0\n" +
+                            "}"));
         }
-    }*/
+    }
 }
