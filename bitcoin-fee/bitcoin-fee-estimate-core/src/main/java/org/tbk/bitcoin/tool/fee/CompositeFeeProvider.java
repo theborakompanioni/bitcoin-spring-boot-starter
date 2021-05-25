44d4491336f7c2287a563aad02c405a74022ed52
theborakompanioni <theborakompanioni+github@gmail.com>
2021-05-26 00:24:15 +0200

    feature: specify scheduler and execute requests in parallel in CompositeFeeProvider

--- bitcoin-fee/bitcoin-fee-estimate-core/src/main/java/org/tbk/bitcoin/tool/fee/CompositeFeeProvider.java
+++ bitcoin-fee/bitcoin-fee-estimate-core/src/main/java/org/tbk/bitcoin/tool/fee/CompositeFeeProvider.java
@@ -3,6 +3,8 @@ package org.tbk.bitcoin.tool.fee;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,12 +17,15 @@ public final class CompositeFeeProvider extends AbstractFeeProvider {
     private final List<FeeProvider> feeProviders;
 
     private final ProviderInfo providerInfo;
+    private final Scheduler scheduler;
 
     public CompositeFeeProvider(List<FeeProvider> feeProviders) {
-        super();
-        requireNonNull(feeProviders);
+        this(feeProviders, Schedulers.parallel());
+    }
 
-        this.feeProviders = ImmutableList.copyOf(feeProviders);
+    public CompositeFeeProvider(List<FeeProvider> feeProviders, Scheduler scheduler) {
+        this.feeProviders = ImmutableList.copyOf(requireNonNull(feeProviders));
+        this.scheduler = requireNonNull(scheduler);
         this.providerInfo = ProviderInfo.SimpleProviderInfo.builder()
                 .name("composite")
                 .description(toDescription(this.feeProviders))
@@ -36,7 +41,9 @@ public final class CompositeFeeProvider extends AbstractFeeProvider {
     protected Flux<FeeRecommendationResponse> requestHook(FeeRecommendationRequest request) {
         return Flux.fromIterable(feeProviders)
                 .filter(provider -> provider.supports(request))
-                .flatMap(provider -> provider.request(request));
+                .parallel().runOn(this.scheduler)
+                .flatMap(provider -> provider.request(request))
+                .sequential();
     }
 
     @Override
