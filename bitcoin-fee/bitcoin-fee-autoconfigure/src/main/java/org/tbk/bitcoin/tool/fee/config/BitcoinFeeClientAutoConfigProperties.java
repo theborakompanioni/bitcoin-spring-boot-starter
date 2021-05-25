fec4501f18be74944e1d2cb434011bd76ee283d8
theborakompanioni <theborakompanioni+github@gmail.com>
2021-05-25 23:43:59 +0200

    fix: ability to enable/disable fee provider via property

--- bitcoin-fee/bitcoin-fee-autoconfigure/src/main/java/org/tbk/bitcoin/tool/fee/config/BitcoinFeeClientAutoConfigProperties.java
+++ bitcoin-fee/bitcoin-fee-autoconfigure/src/main/java/org/tbk/bitcoin/tool/fee/config/BitcoinFeeClientAutoConfigProperties.java
@@ -8,7 +8,9 @@ import org.springframework.validation.Validator;
 @Data
 @ConfigurationProperties(
         prefix = "org.tbk.bitcoin.tool.fee",
-        ignoreUnknownFields = false
+        // do not throw exceptions on unknown keys:
+        // this enables sub-keys for every client, e.g. "org.tbk.bitcoin.tool.fee.mempoolspace.enabled"
+        ignoreUnknownFields = true
 )
 public class BitcoinFeeClientAutoConfigProperties implements Validator {
 
