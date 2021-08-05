6ab1caa8f268d7ee20e5fd8010e5c7b418a2adac
theborakompanioni <theborakompanioni+github@gmail.com>
2021-08-05 22:20:02 +0200

    test: add test for SimpleLinking key in module lnurl-simple

---
+++ incubator/lnurl-java/lnurl-simple/src/test/java/org/tbk/lnurl/simple/auth/SimpleLinkingKeyTest.java
@@ -0,0 +1,43 @@
+package org.tbk.lnurl.simple.auth;
+
+import fr.acinq.secp256k1.Secp256k1Exception;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class SimpleLinkingKeyTest {
+
+    @Test
+    void fromHexStrictSuccess() {
+        String validLinkingKey = "0465d6177992064a24c24213230a0c3eeb5f2047c7286391c7ead608cda473f787af9afcae9af3a6a84f28a775ad257dbf6027448461455aaf482569237dda27bd";
+        SimpleLinkingKey linkingKey = SimpleLinkingKey.fromHexStrict(validLinkingKey);
+
+        assertThat(linkingKey.toHex(), is(validLinkingKey));
+    }
+
+    @Test
+    void fromHexStrictFail() {
+        String validLookingLinkingKey = "04" + "00".repeat(64); // looks valid but is not on sek256k1
+        Secp256k1Exception e = Assertions.assertThrows(Secp256k1Exception.class, () -> SimpleLinkingKey.fromHexStrict(validLookingLinkingKey));
+
+        assertThat(e.getMessage(), is("secp256k1_ec_pubkey_parse failed"));
+    }
+
+    @Test
+    void fromHexLaxSuccess() {
+        String validLinkingKey = "04c8f8a0c04f84ecfe7bf11d818efe67eeefd5bc25aafac437e03b63ce0aaf8a15c930640941add57453019c45f6000a002d2496aa3f30de8795a75dc1266e651c";
+        SimpleLinkingKey linkingKey = SimpleLinkingKey.fromHexLax(validLinkingKey);
+
+        assertThat(linkingKey.toHex(), is(validLinkingKey));
+    }
+
+    @Test
+    void fromHexLaxSuccessMinimal() {
+        String validLookingLinkingKey = "04" + "00".repeat(64); // looks valid but is not on sek256k1
+        SimpleLinkingKey linkingKey = SimpleLinkingKey.fromHexLax(validLookingLinkingKey);
+
+        assertThat(linkingKey.toHex(), is(validLookingLinkingKey));
+    }
+}
