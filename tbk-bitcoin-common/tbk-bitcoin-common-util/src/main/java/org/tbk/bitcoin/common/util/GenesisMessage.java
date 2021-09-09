0fdaa4d0e651a83c5fd864c25da19cdce73f822c
theborakompanioni <theborakompanioni+github@gmail.com>
2021-09-10 00:13:40 +0200

    chore: replace link to blockexplorer with cli command

--- tbk-bitcoin-common/tbk-bitcoin-common-util/src/main/java/org/tbk/bitcoin/common/util/GenesisMessage.java
+++ tbk-bitcoin-common/tbk-bitcoin-common-util/src/main/java/org/tbk/bitcoin/common/util/GenesisMessage.java
@@ -1,6 +1,9 @@
 package org.tbk.bitcoin.common.util;
 
-// see https://blockchair.com/bitcoin/block/0
+// ```shell
+// bitcoin-cli getblock $(bitcoin-cli getblockhash 0) 0 | awk '{print substr($0,263,138)}' | xxd -r -p
+// > The Times 03/Jan/2009 Chancellor on brink of second bailout for banks
+// ```
 public final class GenesisMessage {
     // OP_PUSHBYTES_4 ffff001d OP_PUSHBYTES_1 04 OP_PUSHBYTES_69 5468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73
     private static final String MESSAGE = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
