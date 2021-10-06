d798437a302c7898b3bf8cbe1200f85426e4a1bb
theborakompanioni <theborakompanioni+github@gmail.com>
2021-10-06 23:19:58 +0200

    example: log lnurl-auth wallet action events when "action" is empty

--- incubator/spring-lnurl/spring-lnurl-auth-example-application/src/main/java/org/tbk/lightning/lnurl/example/security/LnurlAuthWalletActionEventListener.java
+++ incubator/spring-lnurl/spring-lnurl-auth-example-application/src/main/java/org/tbk/lightning/lnurl/example/security/LnurlAuthWalletActionEventListener.java
@@ -14,24 +14,11 @@ class LnurlAuthWalletActionEventListener implements ApplicationListener<LnurlAut
 
     @Override
     public void onApplicationEvent(LnurlAuthWalletActionEvent event) {
-        LnurlAuth.Action action = event.getAction().orElse(null);
-
         URI callbackUrl = event.getLnurlAuth().toLnurl().toUri();
-        if (action != null) {
-            switch (action) {
-                case LOGIN:
-                    log.info("Got LOGIN action: {}", callbackUrl);
-                    break;
-                case REGISTER:
-                    log.info("Got REGISTER action: {}", callbackUrl);
-                    break;
-                case LINK:
-                    log.info("Got LINK action: {}", callbackUrl);
-                    break;
-                case AUTH:
-                    log.info("Got AUTH action: {}", callbackUrl);
-                    break;
-            }
-        }
+        String action = event.getAction()
+                .map(LnurlAuth.Action::getValue)
+                .orElse("<empty>");
+
+        log.info("Received lnurl-auth wallet action ({}) event: {}", action, callbackUrl);
     }
 }
