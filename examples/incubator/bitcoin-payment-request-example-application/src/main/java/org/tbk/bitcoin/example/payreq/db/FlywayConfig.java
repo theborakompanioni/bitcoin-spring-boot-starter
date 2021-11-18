fe4baa236adfb27388d07cd64eeabbbe78f1c072
theborakompanioni <theborakompanioni+github@gmail.com>
2021-11-18 11:06:43 +0100

    test: use in-memory sqlite database in test profiles

---
+++ examples/incubator/bitcoin-payment-request-example-application/src/main/java/org/tbk/bitcoin/example/payreq/db/FlywayConfig.java
@@ -0,0 +1,24 @@
+package org.tbk.bitcoin.example.payreq.db;
+
+import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.api.migration.JavaMigration;
+import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    public FlywayConfigurationCustomizer flywayConfigurationCustomizer(ApplicationContext applicationContext) {
+        return configuration -> {
+            JavaMigration[] javaMigrations = applicationContext.getBeansOfType(JavaMigration.class)
+                    .values().toArray(JavaMigration[]::new);
+
+            configuration.javaMigrations(javaMigrations);
+        };
+    }
+
+}
