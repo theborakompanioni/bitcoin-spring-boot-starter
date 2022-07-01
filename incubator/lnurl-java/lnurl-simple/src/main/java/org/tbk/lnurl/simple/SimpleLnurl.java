05033fb491ab735bb29540fb13f7942beaebd218
theborakompanioni <theborakompanioni+github@gmail.com>
2022-07-02 01:42:34 +0200

    upgrade: update bitcoin-kmp from v0.8.2 to v0.8.4

--- incubator/lnurl-java/lnurl-simple/src/main/java/org/tbk/lnurl/simple/SimpleLnurl.java
+++ incubator/lnurl-java/lnurl-simple/src/main/java/org/tbk/lnurl/simple/SimpleLnurl.java
@@ -15,8 +15,8 @@ public class SimpleLnurl implements Lnurl {
     private static final String HUMAN_READABLE_PART = "lnurl";
 
     public static SimpleLnurl fromBech32(String lnurl) {
-        Triple<String, Byte[], Bech32.Encoding> decode = Bech32.decode(lnurl);
-        String decodedLnUrl = new String(Bech32.five2eight(decode.component2(), 0), StandardCharsets.UTF_8);
+        Triple<String, byte[], Bech32.Encoding> decode = Bech32.decodeBytes(lnurl, false);
+        String decodedLnUrl = new String(decode.component2(), StandardCharsets.UTF_8);
 
         return fromUri(URI.create(decodedLnUrl));
     }
@@ -57,14 +57,6 @@ public class SimpleLnurl implements Lnurl {
 
     private static String toBech32(URI uri) {
         byte[] data = uri.toString().getBytes(StandardCharsets.UTF_8);
-        return Bech32.encode(hrp(), toPrimitives(Bech32.eight2five(data)), Bech32.Encoding.Bech32);
-    }
-
-    private static byte[] toPrimitives(Byte[] oBytes) {
-        byte[] bytes = new byte[oBytes.length];
-        for (int i = 0; i < oBytes.length; i++) {
-            bytes[i] = oBytes[i];
-        }
-        return bytes;
+        return Bech32.encodeBytes(hrp(), data, Bech32.Encoding.Bech32);
     }
 }
