d168a8463c9cdec02cac648ac85070c33ed59d37
theborakompanioni <theborakompanioni+github@gmail.com>
2023-07-02 17:09:11 +0200

    feat(example): add run configuration for jsonrpc client example app

--- bitcoin-jsonrpc-client/bitcoin-jsonrpc-client-example-application/src/main/java/org/tbk/bitcoin/jsonrpc/example/BitcoinJsonrpcClientExampleApplication.java
+++ bitcoin-jsonrpc-client/bitcoin-jsonrpc-client-example-application/src/main/java/org/tbk/bitcoin/jsonrpc/example/BitcoinJsonrpcClientExampleApplication.java
@@ -24,7 +24,6 @@ public class BitcoinJsonrpcClientExampleApplication {
                 .sources(BitcoinJsonrpcClientExampleApplication.class)
                 .listeners(applicationPidFileWriter(), webServerPortFileWriter())
                 .web(WebApplicationType.SERVLET)
-                .profiles("development", "local")
                 .run(args);
     }
 
