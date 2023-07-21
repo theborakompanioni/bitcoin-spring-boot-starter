a41902dc9226b1d7343aff9a697efed4b02b097c
theborakompanioni <theborakompanioni+github@gmail.com>
2023-07-21 08:00:13 +0200

    cleanup(exmaple): remove duckdca example app

--- examples/incubator/bitcoin-duckdca-example-application/src/main/java/org/tbk/bitcoin/duckdca/example/BitcoinDuckDcaExampleApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package org.tbk.bitcoin.duckdca.example;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
-
-@Slf4j
-@Configuration(proxyBeanMethods = false)
-class BitcoinDuckDcaExampleApplicationConfig {
-}
