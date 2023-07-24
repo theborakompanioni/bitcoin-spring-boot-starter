50c343a7388c1d838b103a23b29fa0ea793ae9c8
theborakompanioni <theborakompanioni+github@gmail.com>
2023-07-24 10:12:12 +0200

    feat(ln): initial version of lightning-regtest modules

---
+++ lightning-regtest/lightning-regtest-setup-devel/src/main/java/org/tbk/lightning/regtest/setup/devel/impl/LocalClnNodeErinRegistrar.java
@@ -0,0 +1,23 @@
+package org.tbk.lightning.regtest.setup.devel.impl;
+
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
+import org.tbk.lightning.cln.grpc.ClnRpcConfig;
+import org.tbk.lightning.cln.grpc.ClnRpcConfigImpl;
+import org.tbk.lightning.regtest.setup.devel.AbstractDevelClnNodeRegistrar;
+
+class LocalClnNodeErinRegistrar extends AbstractDevelClnNodeRegistrar {
+
+    @Override
+    protected String beanNamePrefix() {
+        return "nodeErin";
+    }
+
+    @Override
+    protected ClnRpcConfig createClnRpcConfig(SslContext sslContext) {
+        return ClnRpcConfigImpl.builder()
+                .host("localhost")
+                .port(19940)
+                .sslContext(sslContext)
+                .build();
+    }
+}
