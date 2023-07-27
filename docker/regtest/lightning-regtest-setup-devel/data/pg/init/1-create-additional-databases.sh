fd0fef9c32afa015635943796414ba7e0d0f0348
theborakompanioni <theborakompanioni+github@gmail.com>
2023-07-27 13:22:14 +0200

    chore(ln): prevent postgres vacuum warnings in lightning regtest env

--- docker/regtest/lightning-regtest-setup-devel/data/pg/init/1-create-additional-databases.sh
+++ docker/regtest/lightning-regtest-setup-devel/data/pg/init/1-create-additional-databases.sh
@@ -5,7 +5,7 @@ set -u
 
 function create_user_and_database() {
 	local database=$1
-	echo "  Creating user and database '$database'"
+	echo "  Creating user and database '$database'..."
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	    CREATE ROLE $database WITH LOGIN ENCRYPTED PASSWORD '$database' VALID UNTIL 'infinity';
 	    CREATE DATABASE $database;
@@ -18,5 +18,5 @@ if [ -n "$POSTGRES_ADDITIONAL_DATABASES" ]; then
 	for db in $(echo $POSTGRES_ADDITIONAL_DATABASES | tr ',' ' '); do
 		create_user_and_database $db
 	done
-	echo "Additional databases created"
+	echo "Additional databases created."
 fi
