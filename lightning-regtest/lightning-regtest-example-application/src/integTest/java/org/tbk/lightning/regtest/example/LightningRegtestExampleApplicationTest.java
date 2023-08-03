24ee1e9fe0f61aaf9413dbd11396ac8579efb5e7
theborakompanioni <theborakompanioni+github@gmail.com>
2023-08-03 19:22:32 +0200

    fix(test): fix setup of lightning-regtest-example-app integration tests

--- lightning-regtest/lightning-regtest-example-application/src/integTest/java/org/tbk/lightning/regtest/example/LightningRegtestExampleApplicationTest.java
+++ lightning-regtest/lightning-regtest-example-application/src/integTest/java/org/tbk/lightning/regtest/example/LightningRegtestExampleApplicationTest.java
@@ -1,4 +1,4 @@
-package org.tbk.bitcoin.regtest.example;
+package org.tbk.lightning.regtest.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
