0edd1d221145c8c6b6eb84d538125d352d205a62
theborakompanioni <theborakompanioni+github@gmail.com>
2023-08-03 15:43:15 +0200

    chore(ln): add 'connect' to LnCommonClient

--- lightning-commons/lightning-commons-client-core/src/main/java/org/tbk/lightning/client/common/core/LnCommonClient.java
+++ lightning-commons/lightning-commons-client-core/src/main/java/org/tbk/lightning/client/common/core/LnCommonClient.java
@@ -1,5 +1,7 @@
 package org.tbk.lightning.client.common.core;
 
+import org.tbk.lightning.client.common.core.proto.ConnectRequest;
+import org.tbk.lightning.client.common.core.proto.ConnectResponse;
 import org.tbk.lightning.client.common.core.proto.GetinfoRequest;
 import org.tbk.lightning.client.common.core.proto.GetinfoResponse;
 import reactor.core.publisher.Mono;
@@ -7,6 +9,8 @@ import reactor.core.publisher.Mono;
 public interface LnCommonClient<T> {
 
     Mono<GetinfoResponse> info(GetinfoRequest request);
+
+    Mono<ConnectResponse> connect(ConnectRequest request);
 
     // TODO: consider removing access to the underlying client after all common functions are implemented
     T baseClient();
