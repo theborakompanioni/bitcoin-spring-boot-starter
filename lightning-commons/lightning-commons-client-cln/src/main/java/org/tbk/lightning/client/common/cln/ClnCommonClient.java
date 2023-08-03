7fc7958878135f00e524807dcb1ae3bc147c6146
theborakompanioni <theborakompanioni+github@gmail.com>
2023-08-03 15:43:15 +0200

    chore(ln): add 'connect' to LnCommonClient

--- lightning-commons/lightning-commons-client-cln/src/main/java/org/tbk/lightning/client/common/cln/ClnCommonClient.java
+++ lightning-commons/lightning-commons-client-cln/src/main/java/org/tbk/lightning/client/common/cln/ClnCommonClient.java
@@ -2,13 +2,15 @@ package org.tbk.lightning.client.common.cln;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.tbk.lightning.client.common.core.LnCommonClient;
-import org.tbk.lightning.client.common.core.proto.Chain;
-import org.tbk.lightning.client.common.core.proto.GetinfoRequest;
-import org.tbk.lightning.client.common.core.proto.GetinfoResponse;
+import org.tbk.lightning.client.common.core.proto.*;
 import org.tbk.lightning.cln.grpc.client.NodeGrpc;
 import reactor.core.publisher.Mono;
 
+import java.util.HexFormat;
+
+@Slf4j
 @RequiredArgsConstructor
 public class ClnCommonClient implements LnCommonClient<NodeGrpc.NodeBlockingStub> {
 
@@ -34,6 +36,27 @@ public class ClnCommonClient implements LnCommonClient<NodeGrpc.NodeBlockingStub
                             .build())
                     .setBlockheight(response.getBlockheight())
                     .build();
+        });
+    }
+
+    @Override
+    public Mono<ConnectResponse> connect(ConnectRequest request) {
+        return Mono.fromCallable(() -> {
+            org.tbk.lightning.cln.grpc.client.ConnectRequest.Builder builder = org.tbk.lightning.cln.grpc.client.ConnectRequest.newBuilder()
+                    .setId(HexFormat.of().formatHex(request.getIdentityPubkey().toByteArray()));
+
+            if (request.hasHost()) {
+                builder.setHost(request.getHost());
+            }
+            if (request.hasPort()) {
+                builder.setPort(request.getPort());
+            }
+
+            org.tbk.lightning.cln.grpc.client.ConnectResponse connectResponse = client.connectPeer(builder.build());
+
+            log.trace("'connectPeer' returned': {}", connectResponse);
+
+            return ConnectResponse.newBuilder().build();
         });
     }
 
--- lightning-commons/lightning-commons-client-lnd/src/main/java/org/tbk/lightning/client/common/lnd/LndCommonClient.java
+++ lightning-commons/lightning-commons-client-lnd/src/main/java/org/tbk/lightning/client/common/lnd/LndCommonClient.java
@@ -3,16 +3,21 @@ package org.tbk.lightning.client.common.lnd;
 import com.google.protobuf.ByteString;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.lightningj.lnd.proto.LightningApi;
 import org.lightningj.lnd.wrapper.SynchronousLndAPI;
+import org.lightningj.lnd.wrapper.message.ConnectPeerRequest;
+import org.lightningj.lnd.wrapper.message.ConnectPeerResponse;
 import org.lightningj.lnd.wrapper.message.GetInfoRequest;
 import org.lightningj.lnd.wrapper.message.GetInfoResponse;
 import org.tbk.lightning.client.common.core.LnCommonClient;
-import org.tbk.lightning.client.common.core.proto.Chain;
-import org.tbk.lightning.client.common.core.proto.GetinfoRequest;
-import org.tbk.lightning.client.common.core.proto.GetinfoResponse;
+import org.tbk.lightning.client.common.core.proto.*;
 import reactor.core.publisher.Mono;
 
+import java.util.HexFormat;
+import java.util.Optional;
+
+@Slf4j
 @RequiredArgsConstructor
 public class LndCommonClient implements LnCommonClient<SynchronousLndAPI> {
 
@@ -40,6 +45,29 @@ public class LndCommonClient implements LnCommonClient<SynchronousLndAPI> {
                             .toList())
                     .setBlockheight(response.getBlockHeight())
                     .build();
+        });
+    }
+
+    @Override
+    public Mono<ConnectResponse> connect(ConnectRequest request) {
+        return Mono.fromCallable(() -> {
+            LightningApi.LightningAddress.Builder addressBuilder = LightningApi.LightningAddress.newBuilder()
+                    .setPubkey(HexFormat.of().formatHex(request.getIdentityPubkey().toByteArray()));
+
+            Optional<String> hostOrEmpty = Optional.of(request.getHost())
+                    .filter(foo -> request.hasHost())
+                    .filter(it -> !it.isBlank())
+                    .map(it -> request.hasPort() ? "%s:%d".formatted(it, request.getPort()) : it);
+
+            hostOrEmpty.ifPresent(addressBuilder::setHost);
+
+            ConnectPeerResponse connectPeerResponse = client.connectPeer(new ConnectPeerRequest(LightningApi.ConnectPeerRequest.newBuilder()
+                    .setAddr(addressBuilder.build())
+                    .build()));
+
+            log.trace("'connectPeer' returned': {}", connectPeerResponse);
+
+            return ConnectResponse.newBuilder().build();
         });
     }
 
