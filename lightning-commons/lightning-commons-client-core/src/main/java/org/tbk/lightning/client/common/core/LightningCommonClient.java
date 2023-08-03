84051a35afac604828d84a796b735d6b11895bb5
theborakompanioni <theborakompanioni+github@gmail.com>
2023-08-03 16:31:53 +0200

    chore(ln): add 'newAddress' to LightningCommonClient

--- lightning-commons/lightning-commons-client-core/src/main/java/org/tbk/lightning/client/common/core/LightningCommonClient.java
+++ lightning-commons/lightning-commons-client-core/src/main/java/org/tbk/lightning/client/common/core/LightningCommonClient.java
@@ -1,15 +1,14 @@
 package org.tbk.lightning.client.common.core;
 
-import org.tbk.lightning.client.common.core.proto.CommonConnectRequest;
-import org.tbk.lightning.client.common.core.proto.CommonConnectResponse;
-import org.tbk.lightning.client.common.core.proto.CommonInfoRequest;
-import org.tbk.lightning.client.common.core.proto.CommonInfoResponse;
+import org.tbk.lightning.client.common.core.proto.*;
 import reactor.core.publisher.Mono;
 
 public interface LightningCommonClient<T> {
     Mono<CommonInfoResponse> info(CommonInfoRequest request);
 
     Mono<CommonConnectResponse> connect(CommonConnectRequest request);
+
+    Mono<CommonNewAddressResponse> newAddress(CommonNewAddressRequest request);
 
     // TODO: consider removing access to the underlying client after all common functions are implemented
     T baseClient();
