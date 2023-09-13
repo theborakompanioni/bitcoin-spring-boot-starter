ee06fdc587ab8248e1e9711e7671b21e0a1be9eb
theborakompanioni <theborakompanioni+github@gmail.com>
2023-09-13 14:42:36 +0200

    refactor: remove generics from LightningCommonClient

--- lightning-regtest/lightning-regtest-setup/src/main/java/org/tbk/lightning/regtest/setup/util/PaymentRouteVerifier.java
+++ lightning-regtest/lightning-regtest-setup/src/main/java/org/tbk/lightning/regtest/setup/util/PaymentRouteVerifier.java
@@ -4,7 +4,7 @@ import org.tbk.lightning.client.common.core.LightningCommonClient;
 import org.tbk.lightning.client.common.core.proto.CommonQueryRouteResponse;
 
 public interface PaymentRouteVerifier {
-    boolean hasDirectRoute(LightningCommonClient<?> origin, LightningCommonClient<?> destination);
+    boolean hasDirectRoute(LightningCommonClient origin, LightningCommonClient destination);
 
     CommonQueryRouteResponse waitForRouteOrThrow(RouteVerification routeVerification);
 }
